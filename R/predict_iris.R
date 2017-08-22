55e0432033e2ddca22dbea18c7212f23f3553a04
jeremiedb <jeremie.desgagne.bouchard@gmail.com>
2017-08-22 17:23:52 -0400

    finalize iris test

--- R/predict_iris.R
+++ R/predict_iris.R
@@ -1,7 +1,7 @@
 
 
-# target_raw <- iris$Species
-# levels(target_raw)
+target_raw <- iris$Species
+iris_levels <- levels(target_raw)
 # target_int <- as.integer(target_raw) - 1
 #
 # total_matrix <- model.matrix(~.-1-Species, data = iris)
@@ -13,13 +13,13 @@
 #               objective = "multi:softprob", eval_metric = "merror")
 # model_iris <- xgb.train(params = param, data = dtrain, nrounds = 400, watchlist = watchlist, print_every_n = 25)
 #
-# devtools::use_data(model_iris, internal = TRUE, overwrite = TRUE)
-
+# devtools::use_data(model_iris, iris_levels, internal = TRUE, overwrite = TRUE)
 
 #' @export
 predict_iris <- function(Sepal.Length = 0, Sepal.Width = 0, Petal.Length = 0, Petal.Width = 0) {
   data_pred <- c(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width)
   data_pred <- matrix(data_pred, nrow = 1)
-  prediction <- predict(object = model_iris, newdata = data_pred)
-  return(prediction)
+  prob <- predict(object = model_iris, newdata = data_pred)
+  label = iris_levels[which.max(prediction)]
+  return(list(label = label, prob = prob))
 }
