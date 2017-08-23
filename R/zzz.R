b6165165837de8cb37f6eb4223a152a4bf94a52a
jeremiedb <jeremie.desgagne.bouchard@gmail.com>
2017-08-23 03:26:29 -0400

    trim out

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
-.onLoad <- function(libname, pkgname){
-  dcgan_symbol <<- mxnet::mx.symbol.load.json(dcgan_model$symbol_json)
-  dcgan_arg.params <<- lapply(dcgan_model$arg.params, mxnet::mx.nd.array)
-  dcgan_aux.params <<- lapply(dcgan_model$aux.params, mxnet::mx.nd.array)
-}
+# .onLoad <- function(libname, pkgname){
+#   dcgan_symbol <<- mxnet::mx.symbol.load.json(dcgan_model$symbol_json)
+#   dcgan_arg.params <<- lapply(dcgan_model$arg.params, mxnet::mx.nd.array)
+#   dcgan_aux.params <<- lapply(dcgan_model$aux.params, mxnet::mx.nd.array)
+# }
